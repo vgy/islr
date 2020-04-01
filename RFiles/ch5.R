9bddbf36d8f785da1ea5ca106a427f703842b0ed
vgy <govindaraj.vijayarangan@gmail.com>
2020-04-01 10:25:02 +0200

    ISLR Stanford MOOC

---
+++ RFiles/ch5.R
@@ -0,0 +1,106 @@
+require(ISLR)
+require(boot)
+?cv.glm
+plot(mpg~horsepower,data=Auto)
+
+## LOOCV
+glm.fit=glm(mpg~horsepower, data=Auto)
+cv.glm(Auto,glm.fit)$delta #pretty slow (doesnt use formula (5.2) on page 180)
+
+##Lets write a simple function to use formula (5.2)
+loocv=function(fit){
+  h=lm.influence(fit)$h
+  mean((residuals(fit)/(1-h))^2)
+}
+
+## Now we try it out
+loocv(glm.fit)
+
+
+cv.error=rep(0,5)
+degree=1:5
+for(d in degree){
+  glm.fit=glm(mpg~poly(horsepower,d), data=Auto)
+  cv.error[d]=loocv(glm.fit)
+}
+plot(degree,cv.error,type="b")
+
+## 10-fold CV
+
+cv.error10=rep(0,5)
+for(d in degree){
+  glm.fit=glm(mpg~poly(horsepower,d), data=Auto)
+  cv.error10[d]=cv.glm(Auto,glm.fit,K=10)$delta[1]
+}
+lines(degree,cv.error10,type="b",col="red")
+
+
+## Bootstrap
+## Minimum risk investment - Section 5.2
+
+alpha=function(x,y){
+  vx=var(x)
+  vy=var(y)
+  cxy=cov(x,y)
+  (vy-cxy)/(vx+vy-2*cxy)
+}
+alpha(Portfolio$X,Portfolio$Y)
+
+## What is the standard error of alpha?
+
+alpha.fn=function(data, index){
+  with(data[index,],alpha(X,Y))
+}
+
+alpha.fn(Portfolio,1:100)
+
+set.seed(1)
+alpha.fn (Portfolio,sample(1:100,100,replace=TRUE))
+
+boot.out=boot(Portfolio,alpha.fn,R=1000)
+boot.out
+plot(boot.out)
+
+
+## Exercise
+load("F:\\MOOC\\Stanford\\ISL\\RFiles\\5.R.RData")
+ls()
+head(Xy)
+tail(Xy)
+dim(Xy)
+names(Xy)
+attach(Xy)
+search()
+summary(lm(y~., data=Xy))
+
+?matplot
+matplot(Xy,type="l")
+
+
+library(boot)
+alpha = function(x,y){
+  vx = var(x)
+  vy = var(y)
+  cxy= cov(x,y)
+  (vy-cxy)/(vx+vy-2*cxy)
+}
+alpha(Xy$X1,Xy$y)
+
+alpha.fn = function(data,index){
+  with(data[index,],alpha(Xy$X1,Xy$y))
+}
+
+alpha.fn<-function(data, index) {
+  fit1<-lm(y~., data=Xy[index,])
+  coefficients(fit1)[['X1']]
+}
+
+set.seed(1)
+alpha.fn (Xy,sample(1:100,100,replace=TRUE))
+
+boot.out=boot(Xy,alpha.fn,R=1000)
+boot.out
+
+?tsboot
+tsboot.out = tsboot(Xy, se_stat, R = 1000, l = 100, sim = "fixed")
+tsboot.out
